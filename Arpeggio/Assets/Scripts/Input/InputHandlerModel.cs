c8790f1659943d6b492a4ad35e146b60919a4d2e
lwkatze <isaac.k.martin@gmail.com>
2017-05-06 11:00:18 -0600

    Initial Creation

---
+++ Arpeggio/Assets/Scripts/Input/InputHandlerModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Input
+{
+	public class InputObject
+	{
+		Touch touch;
+		KeyCode keyCode;
+
+		float axis;
+
+		public InputObject (KeyCode code = KeyCode.None)
+		{
+			touch = new Touch();
+			keyCode = KeyCode.None;
+		}
+	}
+
+	public class InputHandlerModel
+	{
+
+	}
+}
